fe621c7098d71d9cfb3d385e8a67d10dcc387a33
Sonu Kumar <sonu@vibrant-info.com>
2022-10-04 19:14:09 +0530

    Learn create class, compile and run java program into VS code

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+// Filename and classname must bs same
+// To complie -> javac HelloWorld.java
+// To run -> java HelloWorld
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+    
+  }
+}
