8e05a7853c344d8b0796e050b1c2cd4eb8c9e8aa
Sonu Kumar <sonu@vibrant-info.com>
2022-10-09 13:01:45 +0530

    Solved Fibonacci series

--- 3.EvenOdd/EvenOdd.java
+++ 3.EvenOdd/EvenOdd.java
@@ -6,12 +6,14 @@ public class EvenOdd {
 
     int num = scn.nextInt();
 
-    int i=0;
-    while (i<=num){
-      if(i%2 == 0) System.out.println(i+ " is Even.");
-      else System.out.println(i+ " is Odd." );
+    int i = 0;
+    while (i <= num) {
+      if (i % 2 == 0)
+        System.out.println(i + " is Even.");
+      else
+        System.out.println(i + " is Odd.");
       i++;
     }
-scn.close();
+    scn.close();
   }
 }
